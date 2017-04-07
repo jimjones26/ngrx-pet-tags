9e3b8e3c20ccb4d0488dd819d0b579b62911cb0b
jimjones26 <jimjones26@gmail.com>
2017-04-07 15:17:49 -0400

    update complete component

--- src/app/pages/complete/complete.component.ts
+++ src/app/pages/complete/complete.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
+import { Store } from '@ngrx/store';
+import { RESET } from './../../core/pet-tag.actions';
+import { PetTag } from './../../core/pet-tag.model';
 
 @Component({
   selector: 'app-complete',
-  templateUrl: './complete.component.html',
-  styleUrls: ['./complete.component.css']
+  templateUrl: './complete.component.html'
 })
-export class CompleteComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class CompleteComponent implements OnInit, OnDestroy {
+  tagState$: Observable<PetTag>;
+  private tagStateSubscription: Subscription;
+  petTag: PetTag;
+  constructor(private store: Store<PetTag>) {
+    this.tagState$ = store.select('petTag');
   }
-
+  ngOnInit() {
+    this.tagStateSubscription = this.tagState$.subscribe((state) => {
+      this.petTag = state;
+    });
+  }
+  ngOnDestroy() {
+    this.tagStateSubscription.unsubscribe();
+  }
+  newTag() {
+    this.store.dispatch({
+      type: RESET
+    });
+  }
 }
