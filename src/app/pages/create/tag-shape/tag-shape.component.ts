cdd981721a3c4d8a4484901d0239bcbcacf9e998
jimjones26 <jimjones26@gmail.com>
2017-04-07 14:37:50 -0400

    fill in tag shape component

--- src/app/pages/create/tag-shape/tag-shape.component.ts
+++ src/app/pages/create/tag-shape/tag-shape.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-tag-shape',
   templateUrl: './tag-shape.component.html',
   styleUrls: ['./tag-shape.component.css']
 })
-export class TagShapeComponent implements OnInit {
-
+export class TagShapeComponent {
+  tagShape: string;
+  @Output() selectShapeEvent = new EventEmitter();
   constructor() { }
-
-  ngOnInit() {
+  selectShape(shape: string) {
+    this.selectShapeEvent.emit(shape);
   }
-
 }
